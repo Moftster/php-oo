584f0a6f0b826403c44532ff796172213c752639
David Moftakhar <david@sellmysnaps.com>
2020-05-11 15:27:50 +0100

    My gender method included.

--- index.php
+++ index.php
@@ -15,11 +15,21 @@ class Person {
     public function sayHello(){
         return "Hello my name is " . $this->firstName . " " . $this->lastName;
     }
+
+    public function myGender(){
+        if($this->gender == 'f') {
+            return "I am a female";
+        } else {
+            return "I am a male";
+        }
+    }
 }
 
 $tom = new Person('Tom', 'Ben', 'm');
+$jane = new Person('Jane', 'Groves', 'f');
 
 echo $tom->sayHello();
+echo $jane->myGender();
 
 echo "\n";
 // echo $tom->gender;
